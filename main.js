fdf4a92772850e140b6f846b3b38cc77c71d36ab
Karolis Grinkus <karolis@adaptagency.com>
2021-11-15 11:27:14 +0200

    Start using preact

--- main.js
+++
@@ -1,5 +0,0 @@
-import './style.css';
-
-document.querySelector(`#app`).innerHTML = `
-  <p>As a <span contenteditable>&lt;type of user></span> I want <span contenteditable>&lt;to achieve goal></span> so that <span contenteditable>&lt;something happens></span></p>
-`;
