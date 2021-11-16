0aed51d5bfdf0c8e4a8a97b5dc1f7e857e107b5e
Toxicom <tomas.98.leo@gmail.com>
2021-11-16 10:28:57 +0200

    Change User Story placeholder

--- src/components/UserStory/UserStory.js
+++ src/components/UserStory/UserStory.js
@@ -2,25 +2,12 @@ import './UserStory.css';
 
 const UserStory = () => (
   <div className="user-story">
-    <h2>autputo taitl</h2>
-    <h4>ce bus autputas</h4>
-    <p>
-      Laboris aliqua dolor labore consequat minim. Nostrud pariatur Lorem
-      incididunt occaecat. Lorem ut voluptate fugiat est enim quis laborum. Amet
-      sunt est labore excepteur sunt tempor duis adipisicing aute cupidatat ex
-      anim. Tempor velit pariatur in exercitation culpa commodo tempor. Ea est
-      est laborum aliqua anim proident commodo mollit ullamco. Ea veniam minim
-      cillum sunt sint eiusmod fugiat esse consectetur esse id duis deserunt.
-      Ullamco sunt enim officia cupidatat ipsum cupidatat sint exercitation sint
-      anim nisi fugiat nostrud laboris. Fugiat proident labore qui voluptate.
-      Laboris sunt velit culpa laboris. Occaecat dolor sit quis Lorem dolore
-      reprehenderit mollit cillum minim eu. Veniam consequat sunt ipsum qui.
-      Minim adipisicing exercitation sunt ex in sint. Enim sit consequat ullamco
-      non. Non sunt ad eiusmod pariatur ad adipisicing occaecat eiusmod aliqua
-      elit qui. Sint adipisicing veniam sit ea aute Lorem amet incididunt
-      laboris anim tempor id. Voluptate qui qui proident mollit commodo occaecat
-      sunt.
-    </p>
+    <h2 className="user-story--title">User Story</h2>
+    <div className="user-story__content">
+      <span className="user-story__content--empty">
+        Waiting for generation...
+      </span>
+    </div>
   </div>
 );
 
