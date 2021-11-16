3b63044f11e16cb3835d04cc68a37b517d155864
Toxicom <tomas.98.leo@gmail.com>
2021-11-16 10:05:18 +0200

    Add a Header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () => (
+  <div className="header">
+    <span className="header__title">UStory 1.0</span>
+  </div>
+);
+
+export default Header;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,7 +1,11 @@
 import './Layout.css';
+import Header from '../Header';
 
 const Layout = ({ children }) => (
-  <div className="layout-wrapper">{children}</div>
+  <div className="layout">
+    <Header />
+    <div className="layout__wrapper">{children}</div>
+  </div>
 );
 
 export default Layout;
