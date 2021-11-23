5c863efb48a65aacf0c33d023a72bbe4422f22ac
Toxicom <tomas.98.leo@gmail.com>
2021-11-23 11:42:29 +0200

    Add Adapt web link to logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,9 @@ const Header = () => (
     <div className="Header__content">
       <span className="Header__title">Task Generator 1.0</span>
       <span className="Header__text">by</span>
-      <Logo className="Header__logo" />
+      <a href="https://adaptagency.com/" target="_blank" rel="noreferrer">
+        <Logo className="Header__logo" />
+      </a>
     </div>
   </div>
 );
