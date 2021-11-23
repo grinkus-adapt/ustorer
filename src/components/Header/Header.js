44b94e879a7749ed9dcce68573374ca8c769658a
Toxicom <tomas.98.leo@gmail.com>
2021-11-23 14:09:32 +0200

    Sidebar Toggle improvements
    -   Move the component to Header.js;
    -   Change Sidebar-toggle classNames from Layout to Header;
    -   Align the toggle button with flex instead of absolute positioning.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,50 @@
 import './Header.css';
 import Logo from '../Logo';
+import Button from '../Button';
+import { useState } from 'preact/hooks';
 
-const Header = () => (
-  <div className="Header">
-    <div className="Header__content">
-      <span className="Header__title">Task Generator 1.0</span>
-      <span className="Header__text">by</span>
-      <a href="https://adaptagency.com/" target="_blank" rel="noreferrer">
-        <Logo className="Header__logo" />
-      </a>
+const Header = () => {
+  const [sidebarState, setSidebarState] = useState(`visible`);
+
+  const toggleSidebar = () => {
+    const draftList = document.querySelector(`.DraftList`);
+    const toggleButton = document.querySelector(`.Header__sidebar-toggle`);
+    if (draftList.getAttribute(`class`) === `DraftList DraftList--hidden`) {
+      draftList.setAttribute(`class`, `DraftList`);
+      toggleButton.setAttribute(`class`, `Button Header__sidebar-toggle`);
+      setSidebarState(`visible`);
+    } else {
+      draftList.setAttribute(`class`, `DraftList DraftList--hidden`);
+      toggleButton.setAttribute(
+        `class`,
+        `Button Header__sidebar-toggle Header__sidebar-toggle--hidden`
+      );
+      setSidebarState(`hidden`);
+    }
+  };
+  return (
+    <div className="Header">
+      <div className="Header__fake-div" />
+      <div className="Header__content">
+        <span className="Header__title">Task Generator 1.0</span>
+        <span className="Header__text">by</span>
+        <a href="https://adaptagency.com/" target="_blank" rel="noreferrer">
+          <Logo className="Header__logo" />
+        </a>
+      </div>
+      <Button
+        className="Button Header__sidebar-toggle"
+        onClick={() => toggleSidebar()}
+      >
+        {sidebarState === `hidden` && (
+          <img src="../../../node_modules/@fortawesome/fontawesome-free/svgs/solid/angle-double-left.svg" />
+        )}
+        {sidebarState === `visible` && (
+          <img src="../../../node_modules/@fortawesome/fontawesome-free/svgs/solid/angle-double-right.svg" />
+        )}
+      </Button>
     </div>
-  </div>
-);
+  );
+};
 
 export default Header;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,9 +1,6 @@
 import './Layout.css';
 import Header from '../Header';
 import DraftList from '../DraftList';
-import Button from '../Button';
-import { useState } from 'preact/hooks';
-
 const Layout = ({
   children,
   draftState,
@@ -11,52 +8,20 @@ const Layout = ({
   dispatch,
   setFormType,
   setIsOutputEmpty,
-}) => {
-  const [sidebarState, setSidebarState] = useState(`visible`);
-
-  const toggleSidebar = () => {
-    const draftList = document.querySelector(`.DraftList`);
-    const toggleButton = document.querySelector(`.Layout__sidebar-toggle`);
-    if (draftList.getAttribute(`class`) === `DraftList DraftList--hidden`) {
-      draftList.setAttribute(`class`, `DraftList`);
-      toggleButton.setAttribute(`class`, `Button Layout__sidebar-toggle`);
-      setSidebarState(`visible`);
-    } else {
-      draftList.setAttribute(`class`, `DraftList DraftList--hidden`);
-      toggleButton.setAttribute(
-        `class`,
-        `Button Layout__sidebar-toggle Layout__sidebar-toggle--hidden`
-      );
-      setSidebarState(`hidden`);
-    }
-  };
-
-  return (
-    <div className="Layout">
-      <Header />
-      <div className="Layout__wrapper">
-        <div className="Layout__content">{children}</div>
-        <DraftList
-          draftState={draftState}
-          setDraftState={setDraftState}
-          dispatch={dispatch}
-          setFormType={setFormType}
-          setIsOutputEmpty={setIsOutputEmpty}
-        />
-        <Button
-          className="Button Layout__sidebar-toggle"
-          onClick={() => toggleSidebar()}
-        >
-          {sidebarState === `hidden` && (
-            <img src="../../../node_modules/@fortawesome/fontawesome-free/svgs/solid/angle-double-left.svg" />
-          )}
-          {sidebarState === `visible` && (
-            <img src="../../../node_modules/@fortawesome/fontawesome-free/svgs/solid/angle-double-right.svg" />
-          )}
-        </Button>
-      </div>
+}) => (
+  <div className="Layout">
+    <Header />
+    <div className="Layout__wrapper">
+      <div className="Layout__content">{children}</div>
+      <DraftList
+        draftState={draftState}
+        setDraftState={setDraftState}
+        dispatch={dispatch}
+        setFormType={setFormType}
+        setIsOutputEmpty={setIsOutputEmpty}
+      />
     </div>
-  );
-};
+  </div>
+);
 
 export default Layout;
