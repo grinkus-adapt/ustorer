37bdf547dc59e75b19de02b1cd98943ced68c13a
Toxicom <tomas.98.leo@gmail.com>
2021-11-29 18:16:18 +0200

    Install vite-plugin-svgr

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite';
+import svgrPlugin from 'vite-plugin-svgr';
 import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  resolve: {
+    alias: {
+      react: `preact/compat`,
+      'react-dom/test-utils': `preact/test-utils`,
+      'react-dom': `preact/compat`,
+      'react/jsx-runtime': `preact/jsx-runtime`,
+    },
+  },
+  plugins: [
+    preact(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+  ],
 });
