ccfa238eec5bee9de8142cd20ae2b2b86ad67114
Toxicom <tomas.98.leo@gmail.com>
2021-12-01 17:26:08 +0200

    PopupModal Animation Prototype

--- src/components/PopupModal/PopupModal.jsx
+++ src/components/PopupModal/PopupModal.jsx
@@ -1,10 +1,33 @@
 import './PopupModal.css';
+import { useSpring, animated, config } from 'react-spring';
 
-const PopupModal = ({ children, trigger }) =>
-  typeof trigger === `number` && (
-    <div className="PopupModal">
-      <div className="PopupModal__container">{children}</div>
-    </div>
+const Modal = ({ children }) => {
+  const opacityAnimation = useSpring({
+    to: { opacity: 1 },
+    from: { opacity: 0 },
+    config: config.molasses,
+  });
+
+  const transformAnimation = useSpring({
+    to: { x: 0, z: 0, rotateY: 0 },
+    from: { x: 720, z: 90, rotateY: 60 },
+    config: config.molasses,
+  });
+
+  return (
+    <animated.div style={opacityAnimation} className="PopupModal">
+      <animated.div
+        style={transformAnimation}
+        className="PopupModal__container"
+      >
+        {children}
+      </animated.div>
+    </animated.div>
   );
+};
+
+// Modal is outside because of React Spring
+const PopupModal = ({ trigger, ...props }) =>
+  typeof trigger === `number` && <Modal {...props} />;
 
 export default PopupModal;
